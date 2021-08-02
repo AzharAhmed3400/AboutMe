8f62639ed76583ed13b217ba31dce8c8dd7a156b
AzharAhmed3400 <64159747+AzharAhmed3400@users.noreply.github.com>
2021-08-02 14:21:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeColor() {
+  document.getElementById("change").style.color = "red";
+}
+
+function navChange() {
+  document.getElementById("l1").style.color = "white";
+  document.getElementById("l2").style.color = "white";
+  document.getElementById("l3").style.color = "white";
+}
+
+function borderLand() {
+  document.getElementById("pl").style.border = "2px solid white";
+}
