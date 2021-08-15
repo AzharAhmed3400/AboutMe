5eff4ed5eac501890d7797901cb3d6f642e2f181
AzharAhmed3400 <64159747+AzharAhmed3400@users.noreply.github.com>
2021-08-15 11:45:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function changeColor() {
-  document.getElementById("change").style.color = "red";
+  document.getElementById("change").style.color = "#ADADFF";
 }
 
 function navChange() {
-  document.getElementById("l1").style.color = "white";
-  document.getElementById("l2").style.color = "white";
-  document.getElementById("l3").style.color = "white";
+  document.getElementById("l1").style.color = "#ADADFF";
+  document.getElementById("l2").style.color = "#ADADFF";
+  document.getElementById("l3").style.color = "#ADADFF";
 }
 
 function borderLand() {
   document.getElementById("pl").style.border = "2px solid white";
+}
+
+function intro() {
+  document.getElementById("intro").innerHTML = "Welcome!";
 }
